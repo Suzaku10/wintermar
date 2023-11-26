8b53c897015ce669285c81482da468454e44ba62
yudhistira.yoga <yudhistiray198@gmail.com>
2023-11-26 10:45:42 +0700

    chore: add generated module

--- lib/utilities/injection/injection.config.dart
+++ lib/utilities/injection/injection.config.dart
@@ -11,24 +11,33 @@
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
+import 'package:shared_preferences/shared_preferences.dart' as _i5;
 
+import '../../infrastructure/core/register_module.dart' as _i6;
 import '../router/app_route_guard.dart' as _i3;
 import '../router/app_router.dart' as _i4;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
-  _i1.GetIt init({
+  Future<_i1.GetIt> init({
     String? environment,
     _i2.EnvironmentFilter? environmentFilter,
-  }) {
+  }) async {
     final gh = _i2.GetItHelper(
       this,
       environment,
       environmentFilter,
     );
+    final registerModule = _$RegisterModule();
     gh.lazySingleton<_i3.AppRouteGuard>(() => _i3.AppRouteGuard());
     gh.lazySingleton<_i4.AppRouter>(
         () => _i4.AppRouter(gh<_i3.AppRouteGuard>()));
+    await gh.lazySingletonAsync<_i5.SharedPreferences>(
+      () => registerModule.prefs,
+      preResolve: true,
+    );
     return this;
   }
 }
+
+class _$RegisterModule extends _i6.RegisterModule {}

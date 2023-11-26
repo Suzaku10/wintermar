4a9d89dff01c9e2ab53e452c07c761666e105c66
yudhistira.yoga <yudhistiray198@gmail.com>
2023-11-26 20:29:43 +0700

    chore: add loading

--- lib/utilities/utilities.dart
+++ lib/utilities/utilities.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/services.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:uuid/uuid.dart';
 import 'package:wintermar/domain/constant/app_colors.dart';
 import 'package:wintermar/utilities/injection/injection.dart';
@@ -27,4 +28,13 @@ String remainingTime({required int timeInSecond}) {
   String minute = min.toString().length <= 1 ? "0$min" : "$min";
   String second = sec.toString().length <= 1 ? "0$sec" : "$sec";
   return "$minute:$second";
+}
+
+void showLoading() => EasyLoading.show(maskType: EasyLoadingMaskType.black);
+
+void dismissLoading() => EasyLoading.isShow ? EasyLoading.dismiss() : null;
+
+void showError(dynamic e) {
+  if (EasyLoading.isShow) dismissLoading();
+  EasyLoading.showError(e.toString());
 }
